0d06dc1c503cfe46dc65c88c722e5ffb9bface0b
Rain <f16nkin@gmail.com>
2021-01-13 22:40:37 +1000

    #15 Router Добавил  в роутер Middleware, а так же pipeline

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import pipeline from './pipeline'
+import AuthMiddleware from './middleware/AuthMiddleware'
+import AccessMiddleware from './middleware/AccessMiddleware'
+import IsLoggedMiddleware from './middleware/IsLoggedMiddleware'
+import store from '../store'
 
 const routes = [
+  //APPLICATION
   {
     path: '/',
-    name: 'appDesktop',
+    name: 'app.desktop',
     component: () => import('@/views/App/Desktop.vue'),
-    meta: { title: 'Рабочий стол', layout: 'App' }
+    meta: {
+      title: 'Рабочий стол',
+      layout: 'App',
+      middleware: [AuthMiddleware]
+    },
   },
   {
     path: '/app/patient-card/cards/:id',
-    name: 'getCard',
+    name: 'get.card',
     component: () => import('../views/App/Card'),
-    meta: { title: 'Карта пациента', layout: 'App' }
+    meta: {
+      title: 'Карта пациента',
+      layout: 'App',
+      middleware: [AuthMiddleware, AccessMiddleware],
+      permission: 'GET_CARD'
+    }
   },
   {
     path: '/app/patient-card/cards',
-    name: 'getCards',
+    name: 'get.cards',
     component: () => import('../views/App/Cards'),
-    meta: { title: 'Поиск карт', layout: 'App' }
+    meta: {
+      title: 'Поиск карт',
+      layout: 'App',
+      middleware: [AuthMiddleware, AccessMiddleware],
+      permission: 'GET_CARDS'
+    },
   },
+  // ADMINISTRATION
   {
     path: '/admin',
     name: 'adminDesktop',
     component: () => import('@/views/Admin/Desktop.vue'),
     meta: { title: 'Рабочий стол', layout: 'Admin' }
+  },
+  // CORE
+  {
+    path: '/auth/doAuth',
+    name: 'core.authenticate',
+    component: () => import('@/views/Core/Auth.vue'),
+    meta: {
+      title: 'Авторизация',
+      layout: 'Auth',
+      middleware: [IsLoggedMiddleware]
+    }
   }
 ]
 
@@ -34,7 +66,26 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
-  next()
+
+  if (!to.meta.middleware) {
+    return next()
+  }
+  const middleware = to.meta.middleware
+
+  const context = {
+    to,
+    from,
+    next,
+    store,
+    router
+  }
+
+
+  return middleware[0]({
+    ...context,
+    next: pipeline(context, middleware, 1)
+  })
+
 })
 
 export default router
---
+++ src/router/pipeline.js
@@ -0,0 +1,18 @@
+function pipeline (context, middleware, index) {
+    const nextMiddleware = middleware[index]
+
+    if(!nextMiddleware){
+        return context.next
+    }
+
+    return () => {
+        const nextPipeline = pipeline(
+            context, middleware, index + 1
+        )
+
+        nextMiddleware({ ...context, next: nextPipeline })
+
+    }
+}
+
+export default pipeline
