67e026a264fb31a39442c11ac3b50cd3cde4ae5e
Rain <f16nkin@gmail.com>
2021-01-18 21:23:47 +1000

    #21 Асинхронные экшены Только для того, чтобы я мог запомнить как я это делал

--- src/store/App/PatientCard/actions.js
+++ src/store/App/PatientCard/actions.js
@@ -2,30 +2,79 @@ import axios from 'axios'
 
 const apiUrl = 'http://192.168.0.6'
 
+const setStorage = (payload, accessToken, refreshToken) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      sessionStorage.setItem('JWT', accessToken)
+      sessionStorage.setItem('JWTExpTime', payload.exp)
+      sessionStorage.setItem('AccountId', payload.accountId)
+      sessionStorage.setItem('AccountPermissions', payload.accountPermissions)
+      sessionStorage.setItem('Talons', payload.talons)
+      localStorage.setItem('RefreshToken', refreshToken)
+      resolve();
+    }, 1000)
+  })
+  /*sessionStorage.setItem('JWT', accessToken)
+  sessionStorage.setItem('JWTExpTime', payload.exp)
+  sessionStorage.setItem('AccountId', payload.accountId)
+  sessionStorage.setItem('AccountPermissions', payload.accountPermissions)
+  sessionStorage.setItem('Talons', payload.talons)
+  localStorage.setItem('RefreshToken', refreshToken)*/
+}
+
+const refreshTokens = async(oldRefreshToken) => {
+  console.log('Start requesting new pare of tokens')
+  let result = await axios.get(`${apiUrl}/api/v1/auth/doRefresh`, { params: {RefreshToken: oldRefreshToken}})
+  console.log('Finish requesting new pare of tokens')
+  let newAccessToken = result.data.AccessToken
+  let newRefreshToken = result.data.RefreshToken
+  let payload = await JSON.parse(atob(newAccessToken.split('.')[1]))
+  console.log('Start setting storage')
+  await setStorage(payload, newAccessToken, newRefreshToken)
+  console.log('Finish setting storage')
+
+}
+
+
 export const getCardAction = ({ commit }, id) => {
-  return axios.get(`${apiUrl}/api/v1/cards/${id}`)
+  let AuthorizationHeader = sessionStorage.getItem('JWT')
+  return axios.get(`${apiUrl}/api/v1/cards/${id}`, { headers: { Authorization: `Bearer ${AuthorizationHeader}` } })
     .then(function (response) {
       return response.data
     }).then(data => {
       commit('GET_CARD', data)
     })
     .catch(function (error) {
-      console.log(error)
+      console.log(error.response)
     })
 }
 
-export const getCardsAction = ({ commit }, payload) => {
-  return axios.get(
-        `${apiUrl}/api/v1/search/cards`, { params: { searchString: payload.searchString, page: payload.page, offset: payload.offset } })
-    .then(function (response) {
-      return response.data
-    }).then(cards => {
-      commit('GET_CARDS', cards)
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
+export const getCardsAction = async({ commit }, payload) => {
+  let currentRefreshToken = localStorage.getItem('RefreshToken')
+  let accessTokenExpTime = sessionStorage.getItem('JWTExpTime');
+  let currentTime = Math.floor(Date.now() / 1000)
+  if (accessTokenExpTime <= currentTime){
+    await refreshTokens(currentRefreshToken)
+    let cards = await axios.get(`${apiUrl}/api/v1/search/cards`, { params: { searchString: payload.searchString, page: payload.page, offset: payload.offset } })
+    commit('GET_CARDS', cards.data)
+  }else{
+      let cards = await axios.get(`${apiUrl}/api/v1/search/cards`, { params: { searchString: payload.searchString, page: payload.page, offset: payload.offset } })
+      commit('GET_CARDS', cards.data)
+  }
 }
+
+/*export const getCardsAction = ({ commit }, payload) => {
+  return axios.get(
+      `${apiUrl}/api/v1/search/cards`, { params: { searchString: payload.searchString, page: payload.page, offset: payload.offset } })
+      .then(function (response) {
+        return response.data
+      }).then(cards => {
+        commit('GET_CARDS', cards)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+}*/
 
 export const updateCardAction = ({ commit, state }, id) => {
   // Переопределение типов, для нормального захода их на сервер
