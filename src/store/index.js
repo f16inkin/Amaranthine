b472baa5e0e33e7aa8dd3f05f26217283722aee0
Rain <f16nkin@gmail.com>
2021-01-21 19:32:38 +1000

    #27 Убрал storage для аутентификации Теперь нет стораджа для аутентификации куда бы записывались данные о том авторизован пользователь или нет, ключи, токены и прочее по причине, того что сторадж обнуляется при обновлении страницы. А хранить состояние стораджа в хранилище браузера смысла нет. Храню в виде открытых ключей только необходимую информацию

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { createStore } from 'vuex'
 
 import card from './App/PatientCard/card'
-import auth from './Core/Auth/auth'
 
 export default createStore({
   modules: {
-    card,
-    auth
+    card
   }
 })
