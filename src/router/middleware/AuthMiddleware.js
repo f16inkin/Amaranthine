946e7ac7baa80479f6fb164812fbe1741c1493c5
Rain <f16nkin@gmail.com>
2021-01-21 19:14:40 +1000

    #23 Переработал Middleware. Изменения в политике доуступа Теперь мидлвар авторизации просто проверяет наличие токена в заголовке сессии. Никакой при этом защиты на доступ к роутам оно не предоставляет по той причине, что тот самый заголовок можно создать в ручную. Защита доступа к данным на текущий момент осуществляется благодаря серверу, который првоеряет права доступа из токена и если проверка прошла возвращает нужны результат. При отрицательной проверке пока ничего не происходит. Далее в плане сделать редирект на предыдущий роут(если присуствует) если сервер отвечает 401, 403 кодом

--- src/router/middleware/AuthMiddleware.js
+++ src/router/middleware/AuthMiddleware.js
@@ -2,6 +2,23 @@ import {doRefresh} from "../../services/RefreshTokens";
 
 export default async function process({next, router}) {
     let currentAccessToken = sessionStorage.getItem('JWT')
+    if (currentAccessToken){
+        return next()
+    }
+    else {
+        let currentRefreshToken = localStorage.getItem('RefreshToken')
+        if (currentRefreshToken){
+            await doRefresh(currentRefreshToken);
+            console.log('Y\'ll be redirecting after 3 seconds')
+            await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+            return router.push({ name: 'app.desktop' })
+        }else{
+            return router.push({ name: 'core.authenticate' })
+        }
+    }
+}
+/*export default async function process({next, router}) {
+    let currentAccessToken = sessionStorage.getItem('JWT')
     let currentRefreshToken = localStorage.getItem('RefreshToken')
     let accessTokenExpTime = sessionStorage.getItem('JWTExpTime');
     let currentTime = Math.floor(Date.now() / 1000)
@@ -25,4 +42,4 @@ export default async function process({next, router}) {
             return router.push({ name: 'core.authenticate' })
         }
     }
-}
+}*/
